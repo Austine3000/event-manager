7c83955c4477dcbdbd906d65bff9b8061218db87
DIRIBE OGBUANYA <diribe3000@gmail.com>
2021-09-07 03:20:09 +0100

    Added changes for validation

--- src/pages/SignUp/validate.js
+++ src/pages/SignUp/validate.js
@@ -1,5 +1,13 @@
 const validate = (values) => {
   const errors = {};
+  if (!values.firstname) {
+    errors.firstname = "Firstname is required";
+  }
+
+  if (!values.lastname) {
+    errors.lastname = "Lastname is required";
+  }
+
   if (!values.username) {
     errors.username = "Username is required";
   }
